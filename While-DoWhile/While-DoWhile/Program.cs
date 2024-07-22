8624f755c0d2ac0cda5b48b69a54c14cbfa1e476
Sefa Saraç <srcsfysf@gmail.com>
2024-07-22 21:15:03 +0300

    Add files via upload

---
+++ While-DoWhile/While-DoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Limit değerini giriniz: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int sayac = 0;
+
+        // while döngüsü kullanarak sayaç 0'dan başlayarak limit değerine kadar 1'er 1'er artırılır
+        while (sayac <= limit)
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            sayac++;
+        }
+
+        // do-while döngüsü de aynı işlevi yapar, ancak döngü koşulu döngünün sonunda kontrol edilir
+        /*
+        Console.Write("Limit değerini giriniz: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int sayac = 0;
+
+        // do-while döngüsü kullanarak sayaç 0'dan başlayarak limit değerine kadar 1'er 1'er artırılır
+        do
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            sayac++;
+        } while (sayac <= limit);
+    }
+}
+        */
+    }
+}
